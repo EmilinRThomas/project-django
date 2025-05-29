67894c54fa27a04bbbfeeb1f762f451346e64bb3
EmilinRThomas <emilinthomas73@gmail.com>
2025-05-29 13:10:35 +0530

    firstchange

--- main/forms.py
+++ main/forms.py
@@ -11,8 +11,22 @@ class PackageForm(forms.ModelForm):
 
 
 class CustomUserCreationForm(UserCreationForm):
-    role = forms.ChoiceField(choices=CustomUser.ROLE_CHOICES)
+    role = forms.ChoiceField(
+        choices=[choice for choice in CustomUser.ROLE_CHOICES if choice[0].lower() != 'admin']
+    )
 
     class Meta:
         model = CustomUser
         fields = ('username', 'email', 'role', 'password1', 'password2')
+
+    def save(self, commit=True):
+        user = super().save(commit=False)
+        user.set_password(self.cleaned_data['password'])
+
+        # Force regular user role
+        user.is_staff = False
+        user.is_superuser = False
+
+        if commit:
+            user.save()
+        return user
